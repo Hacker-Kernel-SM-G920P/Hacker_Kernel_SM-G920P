7420dev/android_kernel_samsung_zero

Add PhilZ Touch recovery 6.59.0
Hybrid between G920F's TWRP.

Signed-off-by: arter97 <qkrwngud825@gmail.com>

build_recovery.sh
@@ -1,0 +1,54 @@
+#!/bin/bash
+export KERNELDIR=`readlink -f .`
+export RAMFS_SOURCE=`readlink -f $KERNELDIR/recovery`
+
+echo "kerneldir = $KERNELDIR"
+echo "ramfs_source = $RAMFS_SOURCE"
+
+RAMFS_TMP="/tmp/arter97-zeroflte-recovery"
+
+echo "ramfs_tmp = $RAMFS_TMP"
+cd $KERNELDIR
+
+if [ "${1}" = "skip" ] ; then
+	echo "Skipping Compilation"
+else
+	echo "Compiling kernel"
+	cp defconfig .config
+	make "$@" || exit 1
+fi
+
+echo "Building new ramdisk"
+#remove previous ramfs files
+rm -rf '$RAMFS_TMP'*
+rm -rf $RAMFS_TMP
+rm -rf $RAMFS_TMP.cpio
+#copy ramfs files to tmp directory
+cp -ax $RAMFS_SOURCE $RAMFS_TMP
+cd $RAMFS_TMP
+
+find . -name '*.sh' -exec chmod 755 {} \;
+
+$KERNELDIR/ramdisk_fix_permissions.sh 2>/dev/null
+
+#clear git repositories in ramfs
+find . -name .git -exec rm -rf {} \;
+find . -name EMPTY_DIRECTORY -exec rm -rf {} \;
+cd $KERNELDIR
+rm -rf $RAMFS_TMP/tmp/*
+
+cd $RAMFS_TMP
+find . | fakeroot cpio -H newc -o | lzop -9 > $RAMFS_TMP.cpio.lzo
+ls -lh $RAMFS_TMP.cpio.lzo
+cd $KERNELDIR
+
+echo "Making new boot image"
+gcc -w -s -pipe -O2 -Itools/libmincrypt -o tools/mkbootimg/mkbootimg tools/libmincrypt/*.c tools/mkbootimg/mkbootimg.c
+tools/mkbootimg/mkbootimg --kernel $KERNELDIR/arch/arm64/boot/Image --dt $KERNELDIR/dtb.img --ramdisk $RAMFS_TMP.cpio.lzo --base 0x10000000 --pagesize 2048 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --second_offset 0x00f00000 -o $KERNELDIR/recovery.img
+if [ "${1}" = "CC=\$(CROSS_COMPILE)gcc" ] ; then
+	dd if=/dev/zero bs=$((29360128-$(stat -c %s recovery.img))) count=1 >> recovery.img
+fi
+
+echo "done"
+ls -al recovery.img
+echo ""

Hacker-Kernel-SM-G920P/Hacker_Kernel_SM-G920P

build_recovery.sh
@@ -1,0 +1,54 @@
+#!/bin/bash
+export KERNELDIR=`readlink -f .`
+export RAMFS_SOURCE=`readlink -f $KERNELDIR/recovery`
+
+echo "kerneldir = $KERNELDIR"
+echo "ramfs_source = $RAMFS_SOURCE"
+
+RAMFS_TMP="/tmp/hacker-zeroflte-recovery"
+
+echo "ramfs_tmp = $RAMFS_TMP"
+cd $KERNELDIR
+
+if [ "${1}" = "skip" ] ; then
+	echo "Skipping Compilation"
+else
+	echo "Compiling kernel"
+	cp defconfig .config
+	make "$@" || exit 1
+fi
+
+echo "Building new ramdisk"
+#remove previous ramfs files
+rm -rf '$RAMFS_TMP'*
+rm -rf $RAMFS_TMP
+rm -rf $RAMFS_TMP.cpio
+#copy ramfs files to tmp directory
+cp -ax $RAMFS_SOURCE $RAMFS_TMP
+cd $RAMFS_TMP
+
+find . -name '*.sh' -exec chmod 755 {} \;
+
+$KERNELDIR/ramdisk_fix_permissions.sh 2>/dev/null
+
+#clear git repositories in ramfs
+find . -name .git -exec rm -rf {} \;
+find . -name EMPTY_DIRECTORY -exec rm -rf {} \;
+cd $KERNELDIR
+rm -rf $RAMFS_TMP/tmp/*
+
+cd $RAMFS_TMP
+find . | fakeroot cpio -H newc -o | lzop -9 > $RAMFS_TMP.cpio.lzo
+ls -lh $RAMFS_TMP.cpio.lzo
+cd $KERNELDIR
+
+echo "Making new boot image"
+gcc -w -s -pipe -O2 -Itools/libmincrypt -o tools/mkbootimg/mkbootimg tools/libmincrypt/*.c tools/mkbootimg/mkbootimg.c
+tools/mkbootimg/mkbootimg --kernel $KERNELDIR/arch/arm64/boot/Image --dt $KERNELDIR/dtb.img --ramdisk $RAMFS_TMP.cpio.lzo --base 0x10000000 --pagesize 2048 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --second_offset 0x00f00000 -o $KERNELDIR/recovery.img
+if [ "${1}" = "CC=\$(CROSS_COMPILE)gcc" ] ; then
+	dd if=/dev/zero bs=$((29360128-$(stat -c %s recovery.img))) count=1 >> recovery.img
+fi
+
+echo "done"
+ls -al recovery.img
+echo ""